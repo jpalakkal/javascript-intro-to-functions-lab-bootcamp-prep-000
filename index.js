2bc0bf08c32546fa8155748c409873919e97263d
James palakkal <jvpalakkal@gmail.com>
2018-08-09 23:45:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function shout(String) {
+  return String.toUpperCase()
+}
+
+function whisper(String) {
+  return String.toLowerCase()
+}
+
+function logShout(String) {
+  
+	console.log(String.toUpperCase())
+}
+
+function logWhisper(String) {
+  
+	console.log(String.toLowerCase())
+}
+
+function sayHiToGrandma(String) {
+  var lowercase = 'hello!'
+  lowercase.toLowerCase() === lowercase
+  
+  return
+
+
